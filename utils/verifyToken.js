15527837fff096139a12856cd2cb0f9b5e5087f5
fantaCan <122952518+fantaCan@users.noreply.github.com>
2023-01-26 20:56:51 -0800

    Delete utils directory

--- utils/verifyToken.js
+++
@@ -1,9 +0,0 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
-
-export default async function (token){
-    const url = "https://discord.com/api/v10/users/@me"
-    const res = await fetch(url, { headers: {authorization: token} } );
-    const data = await res.json();
-    return data;
-}
